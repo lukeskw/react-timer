6d8fc28a3f09640e965722c883abb792866e4f81
lukeskw <lucasporfirioa@gmail.com>
2024-02-28 11:49:48 -0300

    ft: setup app core and styled components themes

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Homepage } from './pages/Home'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/themes/global'
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <main>
+        <Homepage />
+      </main>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
